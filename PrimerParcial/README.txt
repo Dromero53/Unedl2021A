1cef24c5de6b5edd4642070e6813430188299984
Dromero53 <81374303+Dromero53@users.noreply.github.com>
2021-04-29 10:31:48 -0500

    Add files via upload

---
+++ PrimerParcial/README.txt
@@ -0,0 +1,13 @@
+1) Descargar el repositorio: https://bitbucket.org/danunora/unedl2021a/src/master/
+2) Copiar la carpeta de "PrimerParcial" que contiene el código para el examen. 
+3) Dentro de la carpeta, seguir las intrucciones del archivo README.
+4) Una vez copiada la carpeta, deberá agregarla a su repositorio local.
+5) Sincroniza su repositorio local con su repositorio remoto en bitbucket.
+6) Ya sincronizado, deberá abrir el proyecto y realizar las siguientes tareas:
+	a) Continue el programa de la ventana de búsqueda de personas por nombre o por su apellido. 
+	b) La ventana deberá regresar el nombre completo de la persona en caso de encontrarla. 
+	c) En caso de no encontrar a la persona, regresar un mensaje de error en un mensaje. 
+	d) La ventana de búsqueda deberá tener la opción de limpiar los campos de búsqueda.
+7) Una vez terminado su código, guardar los cambios en su repositorio local.
+8) Sincronizar el repositorio local con el respositorio remoto.
+9) Proporcionar la liga a su repositorio en la respuesta de la pregunta.
---
+++ PrimerParcial/WindowsFormsApp1/WindowsFormsApp1/FormaCaptura.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class FormaCaptura : Form
+    {
+        string[,] personas = new string[,]
+        { { "Angel","Duran" },
+          { "Christopher","Villalobos" },
+          { "Daniel","Lopez" },
+          { "Daniel","Vazquez" },
+          { "Edgar","Banuelos" },
+          { "Fernando","Hernandez" },
+          { "Francisco","Garcia" },
+          { "Ivan","Narvaez" },
+          { "Joel","Juarez" },
+          { "Juan","Romero" },
+          { "Kevin","Gonzalez" },
+          { "Luis","Gomez" },
+          { "Manuel","Mariscal" },
+          { "Mario","Mercado" },
+          { "Marisol","Benitez" },
+          { "Mauricio","Castaneda" },
+          { "Oscar","Ochoa" },
+          { "Yahayra","Rodriguez" },
+          { "Cesar", "De la Cruz" }
+        };
+
+        public FormaCaptura()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (personas.Equals(txtBuscaNombre)&&personas.Equals(txtBuscaApellido))
+            {
+                MessageBox.Show("El nombre existe");
+            } 
+            else
+            {
+                MessageBox.Show("ERROR");
+            }
+            
+
+          
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtBuscaNombre.Text = "";
+            txtBuscaApellido.Text = "";
+        }
+
+        private void txtBuscaNombre_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
